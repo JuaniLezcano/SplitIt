864ab8cdf1db196a837ce527d0a9a48b5644d8a4
JuaniLezcano <tky.juani@gmail.com>
2025-05-12 08:23:28 -0300

    feat: adding DbContext

--- src/SplitIt.Persistence/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace SplitIt.Persistence;
-
-public class Class1
-{
-
-}
---
+++ src/SplitIt.Persistence/SplitItDbContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SplitIt.Domain.Entities;
+
+namespace SplitIt.Persistence;
+
+public class SplitItDbContext : DbContext
+{
+    public SplitItDbContext(DbContextOptions<SplitItDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<User> Users {  get; set; }
+    public DbSet<Expense> Expenses { get; set; }
+    public DbSet<Group> Groups { get; set; }
+    public DbSet<Payment> Payments { get; set; }
+    public DbSet<UserGroup> UserGroups { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<UserGroup>()
+            .HasKey(ug => new { ug.UserId, ug.GroupId });
+
+        modelBuilder.Entity<UserGroup>()
+            .HasOne(ug => ug.User)
+            .WithMany(u => u.UserGroups)
+            .HasForeignKey(ug => ug.UserId);
+
+        modelBuilder.Entity<UserGroup>()
+            .HasOne(ug => ug.Group)
+            .WithMany(g => g.UserGroups)
+            .HasForeignKey(ug => ug.GroupId);
+
+        modelBuilder.Entity<Expense>()
+            .Property(e => e.Type)
+            .HasConversion<string>();
+    }
+}
