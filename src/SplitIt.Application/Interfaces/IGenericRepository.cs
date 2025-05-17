62f8bc53e8f5b375e84f6961ab89c36e009aa973
JuaniLezcano <tky.juani@gmail.com>
2025-05-17 17:28:06 -0300

    feat: add IGenericRepository and IUserRepository interfaces

---
+++ src/SplitIt.Application/Interfaces/IGenericRepository.cs
@@ -0,0 +1,12 @@
+﻿using SplitIt.Domain.Common;
+
+namespace SplitIt.Application.Interfaces
+{
+    public interface IGenericRepository<T> where T : BaseEntity
+    {
+        Task<T?> GetByIdAsync(Guid id);
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(Guid id);
+    }
+}
---
+++ src/SplitIt.Application/Interfaces/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using SplitIt.Domain.Entities;
+
+namespace SplitIt.Application.Interfaces
+{
+    public interface IUserRepository : IGenericRepository<User>
+    {
+        Task<User?> GetByEmailAsync(string email);
+    }
+}
