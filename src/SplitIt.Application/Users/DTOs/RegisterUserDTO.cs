80b56bd53d743d2fa7e0106d5270daf3677dafd9
JuaniLezcano <tky.juani@gmail.com>
2025-05-17 17:32:44 -0300

    feat: adding register interactor and DTO

---
+++ src/SplitIt.Application/Users/DTOs/RegisterUserDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace SplitIt.Application.Users.DTOs
+{
+    public class RegisterUserDTO
+    {
+        public string Name { get; set; } = default!;
+        public string Email { get; set; } = default!;
+        public string Password { get; set; } = default!;
+    }
+}
---
+++ src/SplitIt.Application/Users/UseCases/RegisterUserInteractor.cs
@@ -0,0 +1,35 @@
+﻿using SplitIt.Application.Interfaces;
+using SplitIt.Domain.Entities;
+using SplitIt.Application.Users.DTOs;
+namespace SplitIt.Application.Users.UseCases
+{
+    public class RegisterUserInteractor
+    {
+        private readonly IUserRepository _userRepository;
+
+        public RegisterUserInteractor(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<Guid> AddAsync(RegisterUserDTO dto)
+        {
+            // Validación simple si el usuario ya existe en el sistema
+            var existingUser = await _userRepository.GetByEmailAsync(dto.Email);
+            if (existingUser is not null)
+                throw new InvalidOperationException("El email ya está registrado");
+
+            var user = new User
+            {
+                Id = Guid.NewGuid(),
+                Name = dto.Name,
+                Email = dto.Email,
+                Password = dto.Password, // Encripta en dominio o aquí según diseño
+                CreatedAt = DateTime.UtcNow
+            };
+
+            await _userRepository.AddAsync(user);
+            return user.Id;
+        }
+    }
+}
