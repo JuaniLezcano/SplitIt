cf83c1973886a59dde2a104021692ddf53f8951b
JuaniLezcano <tky.juani@gmail.com>
2025-05-17 17:29:13 -0300

    feat: add GenericRepository and UserRepository implementation

---
+++ src/SplitIt.Persistence/Repositories/GenericRepository.cs
@@ -0,0 +1,42 @@
+﻿using SplitIt.Application.Interfaces;
+using SplitIt.Domain.Common;
+
+namespace SplitIt.Persistence.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+    {
+        private readonly SplitItDbContext _context;
+
+        public GenericRepository(SplitItDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<T?> GetByIdAsync(Guid id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+        public async Task UpdateAsync(T entity)
+        {
+            _context.Set<T>().Update(entity);
+            await _context.SaveChangesAsync();
+        }
+        public async Task DeleteAsync(Guid id)
+        {
+            var entity = await _context.Set<T>().FindAsync(id);
+            if (entity == null)
+                return;
+                //throw new NotFoundException($"{typeof(T).Name} with id {id} not found");
+
+            _context.Set<T>().Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+
+}
---
+++ src/SplitIt.Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SplitIt.Application.Interfaces;
+using SplitIt.Domain.Entities;
+
+namespace SplitIt.Persistence.Repositories
+{
+    public class UserRepository : GenericRepository<User>, IUserRepository
+    {
+        private readonly SplitItDbContext _context;
+
+        public UserRepository(SplitItDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public Task<User?> GetByEmailAsync(string email)
+        {
+            return _context.Users.AsNoTracking().FirstOrDefaultAsync(u => u.Email == email);
+        }
+
+    }
+}
