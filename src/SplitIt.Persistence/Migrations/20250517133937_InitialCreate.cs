64e325727ab9c2ba7fc17c87f1b49586676d0ebf
JuaniLezcano <tky.juani@gmail.com>
2025-05-17 11:10:25 -0300

    migrations: init and second migrations

---
+++ src/SplitIt.Persistence/Migrations/20250517133937_InitialCreate.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SplitIt.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Groups",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Groups", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Expenses",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    CreatedById = table.Column<Guid>(type: "uuid", nullable: false),
+                    Amount = table.Column<decimal>(type: "numeric", nullable: false),
+                    GroupId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Type = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Expenses", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Expenses_Groups_GroupId",
+                        column: x => x.GroupId,
+                        principalTable: "Groups",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Expenses_Users_CreatedById",
+                        column: x => x.CreatedById,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserGroups",
+                columns: table => new
+                {
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    GroupId = table.Column<Guid>(type: "uuid", nullable: false),
+                    IsAdmin = table.Column<bool>(type: "boolean", nullable: false),
+                    IsInvitationAccepted = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserGroups", x => new { x.UserId, x.GroupId });
+                    table.ForeignKey(
+                        name: "FK_UserGroups_Groups_GroupId",
+                        column: x => x.GroupId,
+                        principalTable: "Groups",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserGroups_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Payments",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ExpenseId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Amount = table.Column<decimal>(type: "numeric", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Payments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Payments_Expenses_ExpenseId",
+                        column: x => x.ExpenseId,
+                        principalTable: "Expenses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Payments_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Expenses_CreatedById",
+                table: "Expenses",
+                column: "CreatedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Expenses_GroupId",
+                table: "Expenses",
+                column: "GroupId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Payments_ExpenseId",
+                table: "Payments",
+                column: "ExpenseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Payments_UserId",
+                table: "Payments",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserGroups_GroupId",
+                table: "UserGroups",
+                column: "GroupId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Payments");
+
+            migrationBuilder.DropTable(
+                name: "UserGroups");
+
+            migrationBuilder.DropTable(
+                name: "Expenses");
+
+            migrationBuilder.DropTable(
+                name: "Groups");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ src/SplitIt.Persistence/Migrations/20250517135001_SecondMigration.cs
@@ -0,0 +1,342 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SplitIt.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class SecondMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Expenses_Groups_GroupId",
+                table: "Expenses");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Expenses_Users_CreatedById",
+                table: "Expenses");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Payments_Expenses_ExpenseId",
+                table: "Payments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Payments_Users_UserId",
+                table: "Payments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserGroups_Groups_GroupId",
+                table: "UserGroups");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserGroups_Users_UserId",
+                table: "UserGroups");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Users",
+                table: "Users");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UserGroups",
+                table: "UserGroups");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Payments",
+                table: "Payments");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Groups",
+                table: "Groups");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Expenses",
+                table: "Expenses");
+
+            migrationBuilder.RenameTable(
+                name: "Users",
+                newName: "User");
+
+            migrationBuilder.RenameTable(
+                name: "UserGroups",
+                newName: "UserGroup");
+
+            migrationBuilder.RenameTable(
+                name: "Payments",
+                newName: "Payment");
+
+            migrationBuilder.RenameTable(
+                name: "Groups",
+                newName: "Group");
+
+            migrationBuilder.RenameTable(
+                name: "Expenses",
+                newName: "Expense");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_UserGroups_GroupId",
+                table: "UserGroup",
+                newName: "IX_UserGroup_GroupId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Payments_UserId",
+                table: "Payment",
+                newName: "IX_Payment_UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Payments_ExpenseId",
+                table: "Payment",
+                newName: "IX_Payment_ExpenseId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Expenses_GroupId",
+                table: "Expense",
+                newName: "IX_Expense_GroupId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Expenses_CreatedById",
+                table: "Expense",
+                newName: "IX_Expense_CreatedById");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_User",
+                table: "User",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UserGroup",
+                table: "UserGroup",
+                columns: new[] { "UserId", "GroupId" });
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Payment",
+                table: "Payment",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Group",
+                table: "Group",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Expense",
+                table: "Expense",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Expense_Group_GroupId",
+                table: "Expense",
+                column: "GroupId",
+                principalTable: "Group",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Expense_User_CreatedById",
+                table: "Expense",
+                column: "CreatedById",
+                principalTable: "User",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Payment_Expense_ExpenseId",
+                table: "Payment",
+                column: "ExpenseId",
+                principalTable: "Expense",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Payment_User_UserId",
+                table: "Payment",
+                column: "UserId",
+                principalTable: "User",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserGroup_Group_GroupId",
+                table: "UserGroup",
+                column: "GroupId",
+                principalTable: "Group",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserGroup_User_UserId",
+                table: "UserGroup",
+                column: "UserId",
+                principalTable: "User",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Expense_Group_GroupId",
+                table: "Expense");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Expense_User_CreatedById",
+                table: "Expense");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Payment_Expense_ExpenseId",
+                table: "Payment");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Payment_User_UserId",
+                table: "Payment");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserGroup_Group_GroupId",
+                table: "UserGroup");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserGroup_User_UserId",
+                table: "UserGroup");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UserGroup",
+                table: "UserGroup");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_User",
+                table: "User");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Payment",
+                table: "Payment");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Group",
+                table: "Group");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Expense",
+                table: "Expense");
+
+            migrationBuilder.RenameTable(
+                name: "UserGroup",
+                newName: "UserGroups");
+
+            migrationBuilder.RenameTable(
+                name: "User",
+                newName: "Users");
+
+            migrationBuilder.RenameTable(
+                name: "Payment",
+                newName: "Payments");
+
+            migrationBuilder.RenameTable(
+                name: "Group",
+                newName: "Groups");
+
+            migrationBuilder.RenameTable(
+                name: "Expense",
+                newName: "Expenses");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_UserGroup_GroupId",
+                table: "UserGroups",
+                newName: "IX_UserGroups_GroupId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Payment_UserId",
+                table: "Payments",
+                newName: "IX_Payments_UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Payment_ExpenseId",
+                table: "Payments",
+                newName: "IX_Payments_ExpenseId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Expense_GroupId",
+                table: "Expenses",
+                newName: "IX_Expenses_GroupId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Expense_CreatedById",
+                table: "Expenses",
+                newName: "IX_Expenses_CreatedById");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UserGroups",
+                table: "UserGroups",
+                columns: new[] { "UserId", "GroupId" });
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Users",
+                table: "Users",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Payments",
+                table: "Payments",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Groups",
+                table: "Groups",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Expenses",
+                table: "Expenses",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Expenses_Groups_GroupId",
+                table: "Expenses",
+                column: "GroupId",
+                principalTable: "Groups",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Expenses_Users_CreatedById",
+                table: "Expenses",
+                column: "CreatedById",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Payments_Expenses_ExpenseId",
+                table: "Payments",
+                column: "ExpenseId",
+                principalTable: "Expenses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Payments_Users_UserId",
+                table: "Payments",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserGroups_Groups_GroupId",
+                table: "UserGroups",
+                column: "GroupId",
+                principalTable: "Groups",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserGroups_Users_UserId",
+                table: "UserGroups",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
