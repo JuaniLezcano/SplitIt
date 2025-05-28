614704c5f84b454b7b8687de47d1d60525c77f0a
JuaniLezcano <tky.juani@gmail.com>
2025-05-28 09:00:46 -0300

    feat: Login DTO and Interactor

---
+++ src/SplitIt.Application/Users/DTOs/LoginUserDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace SplitIt.Application.Users.DTOs
+{
+    public class LoginUserDTO
+    {
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+    }
+}
---
+++ src/SplitIt.Application/Users/UseCases/LoginUserInteractor.cs
@@ -0,0 +1,30 @@
+﻿using SplitIt.Application.Interfaces;
+using SplitIt.Application.Users.DTOs;
+
+namespace SplitIt.Application.Users.UseCases
+{
+    public class LoginUserInteractor
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IAuthService _authService;
+
+        public LoginUserInteractor(IUserRepository userRepository, IAuthService authService)
+        {
+            _userRepository = userRepository;
+            _authService = authService;
+        }
+
+        public async Task<Guid> LoginAsync(LoginUserDTO dto)
+        {
+            var user = await _userRepository.GetByEmailAsync(dto.Email);
+            if (user is null)
+                throw new InvalidOperationException("Email o contraseña inválidos");
+
+            var isValid = _authService.VerifyPassword(user, user.Password, dto.Password);
+            if (!isValid)
+                throw new InvalidOperationException("Email o contraseña inválidos");
+
+            return user.Id;
+        }
+    }
+}
